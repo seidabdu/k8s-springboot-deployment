f3e6b14aaf83abf1348f86167631f2332882d9a2
seidabdu <seidpeace@gmail.com>
2021-03-29 10:43:46 -0400

    Adding liveliness and readiness prob .

--- k8s-spring-deployment/src/main/java/seid/demo/kube/k8sspringdeployment/controllers/HomeController.java
+++ k8s-spring-deployment/src/main/java/seid/demo/kube/k8sspringdeployment/controllers/HomeController.java
@@ -1,14 +1,36 @@
 package seid.demo.kube.k8sspringdeployment.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@Value("${hello.greeting:wellcome seid abdu}")
+	String value;
+
+	@Value("${app.config}")
+	String externalConfig;
+	
+	@Value("${app.environmentVariable}")
+	String environmentVariable;
+
+	@Value("${user.name}")
+	String username;
+
+	@Value("${user.password}")
+	String password;
+
+	@GetMapping("/cred")
+	public String getCredentials() {
+		
+		return username + password;
+	}
+
 	@GetMapping("/home/k8s")
 	public String welcome() {
-		return "welcome to the first k8s springboot project";
+		return value + externalConfig + environmentVariable;
 	}
 
 }
